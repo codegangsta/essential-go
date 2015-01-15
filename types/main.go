06300cc85dd0b1096d5c9f36da3dde9f37962c75
Jeremy Saenz <jeremy.saenz@gmail.com>
2015-01-15 15:27:57 -0800

    Added an example for types and structs

--- types/main.go
+++ types/main.go
@@ -1,4 +1,32 @@
 package main
 
+import "fmt"
+
+type point struct {
+	x, y int
+}
+
+type rect struct {
+	pos    point
+	width  int
+	height int
+}
+
+func (r rect) area() int {
+	return r.width * r.height
+}
+
 func main() {
+	p := point{20, 40}
+	fmt.Println(p)
+
+	p = point{x: 20, y: 40}
+	fmt.Println(p)
+
+	r := rect{
+		pos:    point{20, 40},
+		width:  200,
+		height: 400,
+	}
+	fmt.Println(r)
 }
