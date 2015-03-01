ef2f3e54c676c67dfe76b5731d9a0e5195310cfc
Jeremy Saenz <jeremy.saenz@gmail.com>
2015-03-01 14:05:41 -0800

    JMS #2: Channels example

--- channels/main.go
+++ channels/main.go
@@ -6,21 +6,37 @@ import (
 )
 
 func main() {
-	q := make(chan string)
+	jobcount := 100
+	workercount := 5
+	jobs := make(chan int)
+	results := make(chan int)
 
-	for i := 1; i <= 5; i++ {
-		go worker(i, q)
+	// Fire up the workers
+	for i := 1; i <= workercount; i++ {
+		go worker(i, jobs, results)
 	}
 
-	for i := 0; i <= 500; i++ {
-		q <- fmt.Sprintf("Message %v", i)
-	}
+	// Send a bunch of jobs over
+	go func() {
+		for i := 0; i < jobcount; i++ {
+			jobs <- i
+		}
+	}()
 
-	time.Sleep(1 * time.Second)
+	// Get the results from all of our jobs
+	for i := 0; i < jobcount; i++ {
+		<-results
+	}
 }
 
-func worker(id int, queue chan string) {
-	for msg := range queue {
-		fmt.Printf("[worker %v] Doing work on '%v'\n", id, msg)
+func worker(id int, jobs chan int, results chan int) {
+	for jobid := range jobs {
+		fmt.Printf("[worker %v] job %v\n", id, jobid)
+
+		// Do some long term processing here
+		time.Sleep(1000 * time.Millisecond)
+
+		// Send the job id to the results
+		results <- jobid
 	}
 }
